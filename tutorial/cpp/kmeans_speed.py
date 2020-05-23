b059fbc799e3ac3f86d394573b30f38adb991299
Yujian <yujian_fu@outlook.com>
2020-05-24 06:11:15 +0800

    add testing kmeans speed

--- tutorial/cpp/kmeans_speed.py
+++ tutorial/cpp/kmeans_speed.py
@@ -11,6 +11,7 @@ niter = 20
 verbose = True
 
 print("This is the GPU version of Kmeans")
+'''
 start = time.time()
 b = a.reshape(-1, d + 4)[:, 4:]
 b = b[random.sample(range(b.shape[0]), 10000), :]
@@ -101,20 +102,19 @@ kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose, gpu=True)
 kmeans.train(b)
 end = time.time()
 print("Dataset size: ", b.shape[0], " Ncentroids: ", ncentroids, " : ", end-start, "\n\n")
-
+'''
 
 start = time.time()
 b = a.reshape(-1, d + 4)[:, 4:]
-b = b[random.sample(range(b.shape[0]), 10000000), :]
+b = b[random.sample(range(b.shape[0]), 50000000), :]
 b = np.ascontiguousarray(b.astype('float32'))
-ncentroids = 100000
+ncentroids = 1000000
 kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose, gpu=True)
 kmeans.train(b)
 end = time.time()
 print("Dataset size: ", b.shape[0], " Ncentroids: ", ncentroids, " : ", end-start, "\n\n")
 
-
-
+'''
 start = time.time()
 b = a.reshape(-1, d + 4)[:, 4:]
 b = b[random.sample(range(b.shape[0]), 100000000), :]
@@ -145,6 +145,7 @@ kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose, gpu=True)
 kmeans.train(b)
 end = time.time()
 print("Dataset size: ", b.shape[0], " Ncentroids: ", ncentroids, " : ", end-start, "\n\n")
+'''
 
 
 
