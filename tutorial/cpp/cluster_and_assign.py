03e838cff2f467e162c3af255ce728354c0dc91c
Yujian <yujian_fu@outlook.com>
2020-05-24 18:00:05 +0800

    add cluster and assign in python

--- tutorial/cpp/cluster_and_assign.py
+++ tutorial/cpp/cluster_and_assign.py
@@ -21,11 +21,11 @@ kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose, gpu=True)
 kmeans.train(b)
 end = time.time()
 print("Dataset size: ", b.shape[0], " Ncentroids: ", ncentroids, " : ", end-start, "\n\n")
-f = open("/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_nc_"+str(ncentroids)+".fvecs", "wb+")
+f = open("/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_nc_"+str(ncentroids)+".fvecs", "wb")
 for i in range(ncentroids):
-    f.write(int(d));
+    f.write(d);
     for j in range(d):
-        f.write(float(kmeans.centroids[i][j]))
+        f.write(kmeans.centroids[i][j])
 f.close()
 
 
@@ -35,7 +35,7 @@ b = a.reshape(-1, d + 4)[:, 4:]
 base_size = b.shape[0]
 num_batches = 100000
 batch_size = base_size / num_batches
-f = open("/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_ids_"+std(batch_size)+".fvecs", "wb+")
+f = open("/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_ids_"+str(batch_size)+".fvecs", "wb")
 print("Saving ids")
 for i in range(10):
     subset = b[range(batch_size*i, batch_size * (i+1)), :]
