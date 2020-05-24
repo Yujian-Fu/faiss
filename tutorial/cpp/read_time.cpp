a3cc6684209af2c58ed3b2653c5a43aa22b85440
Yujian <yujian_fu@outlook.com>
2020-05-25 07:15:49 +0800

    add read_time to test random read speed

--- tutorial/cpp/read_time.cpp
+++ tutorial/cpp/read_time.cpp
@@ -31,7 +31,7 @@ void readXvec(std::ifstream &in, T *data, const size_t d, const size_t n = 1)
 {
     uint32_t dim = d;
     for (size_t i = 0; i < n; i++) {
-        in.seekg(sizeof(uint32_t)+sizeof(T)*dim), std::ios::beg);
+        in.seekg((sizeof(uint32_t)+sizeof(T)*dim), std::ios::beg);
         in.read((char *) &dim, sizeof(uint32_t));
         if (dim != d) {
             std::cout << "file error\n";
