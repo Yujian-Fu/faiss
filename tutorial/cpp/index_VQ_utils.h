af5084dc0b203b16c15dd4265a9d2008661b8f08
Yujian <yujian_fu@outlook.com>
2020-05-31 14:43:59 +0800

    add VQ test

--- tutorial/cpp/index_VQ_utils.h
+++ tutorial/cpp/index_VQ_utils.h
@@ -29,7 +29,7 @@ class StopW{
 
 template<typename T>
 void PrintVector(T *data, const size_t dimension){
-    std::cout << "Printing sample (2 vectors) of the dataset " << std::endl;
+    std::cout << "Printing sample (1 vector) of the dataset " << std::endl;
     for (size_t i= 0; i < 1 * dimension; i++)
     {
         std::cout << (float)data[i];
