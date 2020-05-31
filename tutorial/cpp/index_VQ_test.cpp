cf49b0b18cd4d69a59bd00bc2b3160fd84fbc13b
Yujian <yujian_fu@outlook.com>
2020-05-31 14:22:30 +0800

    add VQ test

--- tutorial/cpp/index_VQ_test.cpp
+++ tutorial/cpp/index_VQ_test.cpp
@@ -71,7 +71,7 @@ int main(){
 
     getrusage(RUSAGE_SELF,&r_usage);
     // Print the maximum resident set size used (in kilobytes).
-    printf("Memory usage: %ld kilobytes\n",r_usage.ru_maxrss);
+    std::cout << "Memory usage: " << r_usage.ru_ixrss << " / " << r_usage.ru_isrss << " / " << r_usage.ru_idrss << " / " << r_usage.ru_maxrss;
 
     //Initialize the index
     
@@ -123,7 +123,8 @@ int main(){
 
     getrusage(RUSAGE_SELF,&r_usage);
     // Print the maximum resident set size used (in kilobytes).
-    printf("Memory usage: %ld kilobytes\n",r_usage.ru_maxrss);
+    // printf("Memory usage: %ld kilobytes\n",r_usage.ru_maxrss);
+    std::cout << "Memory usage: " << r_usage.ru_ixrss << " / " << r_usage.ru_isrss << " / " << r_usage.ru_idrss << " / " << r_usage.ru_maxrss;
 
     //Assign all base vectors
     if (!exists(path_idxs)){
@@ -184,8 +185,8 @@ int main(){
 
     getrusage(RUSAGE_SELF,&r_usage);
     // Print the maximum resident set size used (in kilobytes).
-    printf("Memory usage: %ld kilobytes\n",r_usage.ru_maxrss);
-
+    // printf("Memory usage: %ld kilobytes\n",r_usage.ru_maxrss);
+    std::cout << "Memory usage: " << r_usage.ru_ixrss << " / " << r_usage.ru_isrss << " / " << r_usage.ru_idrss << " / " << r_usage.ru_maxrss;
     /*
     //Search
     std::cout << "Start Searching " << std::endl;
