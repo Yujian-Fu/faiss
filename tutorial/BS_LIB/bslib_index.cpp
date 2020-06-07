a5a407e95742ed39144f370f8ab28563a053dc43
Yujian <yujian_fu@outlook.com>
2020-06-08 03:59:04 +0800

    Finished LQ and VQ

--- tutorial/BS_LIB/bslib_index.cpp
+++ tutorial/BS_LIB/bslib_index.cpp
@@ -619,8 +619,8 @@ namespace bslib{
 
         input.read((char *) & final_nc_input, sizeof(size_t));
         assert(final_nc_input == this->final_nc);
-        std::cout << "The input final nc is" << final_nc_input << std::endl;
-        std::cout << "The norm code size is " << this->norm_code_size;
+        std::cout << "The input final_nc is "  << final_nc_input << std::endl;
+        std::cout << "The norm code size is " << this->norm_code_size <<std::endl;;
         for (size_t i = 0; i < this->final_nc; i++){
             input.read((char *) & group_size_input, sizeof(size_t));
             input.read((char *) base_norm_codes[i].data(), group_size_input * norm_code_size * sizeof(uint8_t));
