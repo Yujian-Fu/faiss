681ec2757183e107cf70704cea3c413825cc987b
Yujian <yujian_fu@outlook.com>
2020-06-14 01:49:08 +0800

    Add search mechanism

--- tutorial/BS_LIB/bslib_index.cpp
+++ tutorial/BS_LIB/bslib_index.cpp
@@ -75,10 +75,10 @@ namespace bslib{
             for (size_t i = 0; i < this->lq_quantizer_index[0].alphas.size(); i++){
                 std::cout << this->lq_quantizer_index[0].alphas[i] << " ";
             }
-            std::cout << "The nn_dists in lq quantizer: " << std::endl;
+            std::cout << "The nn_ids in lq quantizer: " << std::endl;
             for (size_t i = 0; i < 1000; i ++){
                 for (size_t j = 0; j < 10; j++){
-                    std::cout << this->lq_quantizer_index[0].nn_centroid_dists[i][j] << " ";
+                    std::cout << this->lq_quantizer_index[0].nn_centroid_idxs[i][j] << " ";
                 }
                 std::cout << std::endl;
             }
