b430bd06dba87b83d87306c34dacaf0bb93bf38f
Yujian <yujian_fu@outlook.com>
2020-06-14 06:43:06 +0800

    Add search mechanism

--- tutorial/BS_LIB/bslib_index.cpp
+++ tutorial/BS_LIB/bslib_index.cpp
@@ -314,7 +314,7 @@ namespace bslib{
             assert(n_lq + n_vq == i);
             size_t group_size;
             if (index_type[i] == "VQ"){
-                //std::cout << "searching in VQ layer" << std::endl;
+                std::cout << "searching in VQ layer" << std::endl;
                 group_size = vq_quantizer_index[n_vq].nc_per_group;
                 result_dists.resize(group_size * n);
                 vq_quantizer_index[n_vq].search_in_group(n, assign_data, group_idxs.data(), result_dists.data());
@@ -328,7 +328,7 @@ namespace bslib{
             }
 
             else if(index_type[i] == "LQ"){
-                //std::cout << "searching in LQ layer" << std::endl;
+                std::cout << "searching in LQ layer" << std::endl;
                 group_size = lq_quantizer_index[n_lq].nc_per_group;
                 result_dists.resize(group_size * n);
                 lq_quantizer_index[n_lq].search_in_group(n, assign_data, queries_upper_centroid_dists, group_idxs.data(), result_dists.data());
@@ -347,14 +347,14 @@ namespace bslib{
             }
 
             if (i < this->layers-1 && index_type[i+1] == "LQ"){
-                //std::cout << "The next layer is LQ, load the query centroid distsnaces" << std::endl;
+                std::cout << "The next layer is LQ, load the query centroid distsnaces" << std::endl;
                 for (size_t j = 0; j < n; j++){
                     for (size_t m = 0; m < group_size; m++)
                         queries_upper_centroid_dists[j].insert(std::pair<idx_t, float>(result_labels[j*group_size+m], result_dists[j*group_size+m]));
                 }
             }
 
-            //std::cout << "Choosing k instances with smallest distances " << std::endl;
+            std::cout << "Choosing k instances with smallest distances " << std::endl;
 
             clock_t starttime = clock();
             for (size_t j = 0; j < n; j++){
