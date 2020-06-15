e51f55cb43c7724f031c7cca42b3f8f925cf6b0d
Yujian <yujian_fu@outlook.com>
2020-06-15 20:41:33 +0800

    Add search mechanism

--- tutorial/BS_LIB/parameters_millions.h
+++ tutorial/BS_LIB/parameters_millions.h
@@ -6,10 +6,10 @@ typedef float origin_data_type;
 /* Parameter setting: */
 //Exp parameters
 //For index initialization
-const size_t layers = 5;
-const std::string index_type[layers] = {"VQ", "VQ", "VQ", "LQ", "LQ"};
-const uint32_t ncentroids[layers] = {10, 10, 10, 10, 100};
-const char * path_quantizers = "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_VQ_LQ_LQ/SIFT1M/quantizer_10_10_10_10_100.qt";
+const size_t layers = 4;
+const std::string index_type[layers] = {"VQ", "VQ", "LQ", "LQ"};
+const uint32_t ncentroids[layers] = {100, 10, 10, 100};
+const char * path_quantizers = "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_VQ_LQ_LQ/SIFT1M/quantizer_100_10_10_100.qt";
 const bool use_subset = false;
 const bool pq_use_subset = true;
 
@@ -33,12 +33,12 @@ const size_t ngt = 100;
 const size_t nq = 10000;
 const size_t result_k = 10;
 const size_t max_vectors = 5000;
-size_t keep_space[layers] = {5, 2, 2, 2, 10};
+size_t keep_space[layers] = {5, 2, 2, 10};
 
 bool is_recording = true;
 
 // Folder path
-const char * folder_model = "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ";
+const char * folder_model = "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_LQ_LQ";
 
 //File paths
 const char * path_learn =     "/home/y/yujianfu/ivf-hnsw/data/SIFT1M/sift_learn.fvecs";
@@ -46,9 +46,9 @@ const char * path_base =      "/home/y/yujianfu/ivf-hnsw/data/SIFT1M/sift_base.f
 const char * path_gt =        "/home/y/yujianfu/ivf-hnsw/data/SIFT1M/sift_groundtruth.ivecs";
 const char * path_query =     "/home/y/yujianfu/ivf-hnsw/data/SIFT1M/sift_query.fvecs";
 
-const char * path_record =    "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_VQ_LQ_LQ/SIFT1M/recording_10_10_10_10_100.txt";
-const char * path_pq =        "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_VQ_LQ_LQ/SIFT1M/PQ16_10_10_10_10_100.pq";
-const char * path_pq_norm =   "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_VQ_LQ_LQ/SIFT1M/PQ_NORM16_10_10_10_10_100.pq";
-const char * path_idxs =      "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_VQ_LQ_LQ/SIFT1M/base_idxs_10_10_10_10_100.ivecs";
-const char * path_index =     "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_VQ_LQ_LQ/SIFT1M/PQ16_10_10_10_10_100.index";
+const char * path_record =    "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_LQ_LQ/SIFT1M/recording_100_10_10_100.txt";
+const char * path_pq =        "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_LQ_LQ/SIFT1M/PQ16_100_10_10_100.pq";
+const char * path_pq_norm =   "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_LQ_LQ/SIFT1M/PQ_NORM16_100_10_10_100.pq";
+const char * path_idxs =      "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_LQ_LQ/SIFT1M/base_idxs_100_10_10_100.ivecs";
+const char * path_index =     "/home/y/yujianfu/ivf-hnsw/models_VQ_VQ_LQ_LQ/SIFT1M/PQ16_100_10_10_100.index";
 
