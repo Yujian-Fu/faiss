39f2a6cabf042a24b90d93dc507a2e4e103e8690
Yujian <yujian_fu@outlook.com>
2020-06-16 01:47:48 +0800

    Add search mechanism

--- tutorial/BS_LIB/parameters_millions.h
+++ tutorial/BS_LIB/parameters_millions.h
@@ -32,7 +32,7 @@ const size_t nbatches = nb / batch_size; //1000
 const size_t ngt = 100;
 const size_t nq = 10000;
 const size_t result_k = 10;
-const size_t max_vectors = 5000;
+const size_t max_vectors = 10000;
 size_t keep_space[layers] = {10, 5, 10};
 
 bool is_recording = true;
