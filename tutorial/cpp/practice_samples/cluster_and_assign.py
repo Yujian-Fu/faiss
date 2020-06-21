6823592115b703fab4745b61eca6b8219d9ba469
Yujian <yujian_fu@outlook.com>
2020-06-21 18:19:09 +0800

    build million centroids for ivf-hnsw

--- tutorial/cpp/practice_samples/cluster_and_assign.py
+++ tutorial/cpp/practice_samples/cluster_and_assign.py
@@ -4,26 +4,32 @@ import time
 import random
 import struct
 
-learn_set = "/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_learn.bvecs"
-base_set = "/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_base.bvecs"
+learn_set = "/home/y/yujianfu/ivf-hnsw/data/SIFT1M/sift_learn.fvecs"
+base_set = "/home/y/yujianfu/ivf-hnsw/data/SIFT1M/sift_base.fvecs"
 
-a = np.memmap(learn_set, dtype='uint8', mode='r')
-d = int(a[:4].view('int32')[0])
+#a = np.memmap(learn_set, dtype='uint8', mode='r')
+#d = int(a[:4].view('int32')[0])
+#b = a.reshape(-1, d + 4)[:, 4:]
+
+a = np.fromfile(learn_set, dtype='float')
+d = a[0]
+b = a.reshape(-1, d + 1)[:, 1:].copy()
+
+
 print("Dataset dimension is ", d)
 niter = 20
 verbose = True
 
 start = time.time()
-b = a.reshape(-1, d + 4)[:, 4:]
-train_size = 10000000
+train_size = 100000
 b = b[random.sample(range(b.shape[0]), train_size), :]
 b = np.ascontiguousarray(b.astype('float32'))
-ncentroids = 150000
+ncentroids = 10000
 kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose, gpu=True)
 kmeans.train(b)
 end = time.time()
-print("Dataset size: ", b.shape[0], " Ncentroids: ", ncentroids, " : ", end-start, "\n\n")
-f = open("/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_nc_"+str(ncentroids)+"_numtrain_" + str(train_size)+".fvecs", "wb")
+print("Dataset size: ", b.shape[0], " Ncentroids: ", ncentroids, " Time for reading: ", end-start, " s \n\n")
+f = open("/home/y/yujianfu/ivf-hnsw/data/SIFT1M/centroids_sift1M.fvecs", "wb")
 for i in range(ncentroids):
     f.write(struct.pack('i', d))
     for j in range(d):
@@ -31,14 +37,18 @@ for i in range(ncentroids):
 f.close()
 
 
-a = np.memmap(base_set, dtype='uint8', mode='r')
-d = a[:4].view('int32')[0]
-b = a.reshape(-1, d + 4)[:, 4:]
+#a = np.memmap(base_set, dtype='uint8', mode='r')
+#d = a[:4].view('int32')[0]
+#b = a.reshape(-1, d + 4)[:, 4:]
+
+a = np.fromfile(base_set, dtype='float')
+d = a[0]
+b = a.reshape(-1, d + 1)[:, 1:].copy()
 base_size = b.shape[0]
 print("Base set size is ", base_size)
 num_batches = 10000
 batch_size = int(base_size / num_batches)
-f = open("/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_ids_"+str(batch_size)+"_nc_" + str(ncentroids) + ".fvecs", "wb")
+f = open("/home/y/yujianfu/ivf-hnsw/data/SIFT1M/precomputed_idxs_sift1M.ivecs", "wb")
 print("Saving ids")
 for i in range(num_batches):
     if (i % 100 == 0):
@@ -48,7 +58,7 @@ for i in range(num_batches):
     D, I = kmeans.index.search(subset, 1)
     f.write(struct.pack('i', batch_size))
     for j in range(I.shape[0]):
-        f.write(struct.pack('f', I[j][0]))
+        f.write(struct.pack('i', I[j][0]))
 f.close()
 
 
