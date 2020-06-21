17925af7ce7b371db4ac9a66b0714b389a44e921
Yujian <yujian_fu@outlook.com>
2020-06-21 18:59:52 +0800

    build million centroids for ivf-hnsw

--- tutorial/BS_LIB/parameters_VQ_LQ.h
+++ tutorial/BS_LIB/parameters_VQ_LQ.h
@@ -8,8 +8,8 @@ typedef uint8_t origin_data_type;
 //For index initialization
 const size_t layers = 2;
 const std::string index_type[layers] = {"VQ", "LQ"};
-const uint32_t ncentroids[layers] = {100000, 100};
-const char * path_quantizers = "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/quantizer_100000_100.qt";;
+const uint32_t ncentroids[layers] = {10000, 100};
+const char * path_quantizers = "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/quantizer_10000_100.qt";;
 const bool use_subset = true;
 const bool pq_use_subset = true;
 const bool use_reranking = true;
@@ -20,7 +20,7 @@ const size_t bytes_per_code = 16;
 const size_t bytes_per_norm_code = 1;
 const size_t nbits = 8; //Or 16
 const size_t nt = 100000000;
-const size_t subnt = 10000000;
+const size_t subnt = 1000000;
 const size_t dimension = 128;
 
 //For assigning ID
@@ -50,9 +50,9 @@ const char * path_base = "/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_base.bvec
 const char * path_gt = "/home/y/yujianfu/ivf-hnsw/data/SIFT1B/gnd/idx_1000M.ivecs";
 const char * path_query = "/home/y/yujianfu/ivf-hnsw/data/SIFT1B/bigann_learn.bvecs";
 
-const char * path_record =    "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/recording_100000_100.txt";
-const char * path_pq =        "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/PQ16_100000_100.pq";
-const char * path_pq_norm =   "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/PQ_NORM16_100000_100.pq";
-const char * path_idxs =      "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/base_idxs_100000_100.ivecs";
-const char * path_index =     "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/PQ16_100000_100.index";
+const char * path_record =    "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/recording_10000_100.txt";
+const char * path_pq =        "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/PQ16_10000_100.pq";
+const char * path_pq_norm =   "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/PQ_NORM16_10000_100.pq";
+const char * path_idxs =      "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/base_idxs_10000_100.ivecs";
+const char * path_index =     "/home/y/yujianfu/ivf-hnsw/models_VQ_LQ/SIFT1B/PQ16_10000_100.index";
 
