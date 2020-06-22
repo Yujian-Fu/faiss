b9917d4ac181db86065bb3085ac2ee66e437e6b9
Yujian <yujian_fu@outlook.com>
2020-06-22 10:05:41 +0800

    build million centroids for ivf-hnsw

--- tutorial/BS_LIB/bslib_index.cpp
+++ tutorial/BS_LIB/bslib_index.cpp
@@ -630,8 +630,8 @@ namespace bslib{
 
                 keep_k_min(search_space, keep_result_space, result_dists.data(), result_labels.data(), group_dists.data(), group_idxs.data());
                 std::cout << "The result labels are: " << std::endl;
-                for (size_t temp = 0; temp < 10000; temp++){
-                    std::cout << result_labels[temp] << " " << result_dists[temp] << " ";
+                for (size_t temp = 0; temp < keep_result_space; temp++){
+                    std::cout << group_idxs[temp] << " " << group_dists[temp] << " ";
                 }
                 exit(0);
                 std::cout << std::endl;
