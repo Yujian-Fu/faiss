85296072ea19ae86a186255f0ba19fe30ab44391
Yujian <yujian_fu@outlook.com>
2020-06-28 02:17:43 +0800

    modify train and cluster

--- tutorial/BS_LIB/bslib_index.cpp
+++ tutorial/BS_LIB/bslib_index.cpp
@@ -121,9 +121,7 @@ namespace bslib{
             this->vq_quantizer_index[0].quantizers[0].search(group_size, this->train_data.data(), 1, centroid_distances.data(), centroid_idxs.data());
             for (size_t j = 0; j < group_size; j++){
                 this->train_data_idxs[j] = centroid_idxs[j];
-                std::cout << this->train_data_idxs[j] << " ";
             }
-            std::cout << std::endl;
 
 
             uint32_t nc_per_group = ncentroids[0];
@@ -131,6 +129,7 @@ namespace bslib{
             std::vector<idx_t> nn_centroids_idxs(ncentroids[0]*ncentroids[1]);
             std::vector<float> nn_centroids_dists(ncentroids[0]*ncentroids[1]);
             vq_quantizer_index[0].compute_nn_centroids(nc_per_group, upper_centroids.data(), nn_centroids_dists.data(), nn_centroids_idxs.data());
+            
             add_lq_quantizer(ncentroids[0], ncentroids[1], upper_centroids.data(), nn_centroids_idxs.data(), nn_centroids_dists.data(), false);
         }
         else
