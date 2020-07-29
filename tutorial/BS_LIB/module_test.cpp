27c440404f9c5fdfef3928b7d9418647be221c88
Yujian <yujian_fu@outlook.com>
2020-07-29 19:58:10 +0800

    fixed bugs

--- tutorial/BS_LIB/module_test.cpp
+++ tutorial/BS_LIB/module_test.cpp
@@ -43,13 +43,16 @@ int main(){
        std::vector<idx_t> origin_ids(nb);
        std::ifstream ids_input(path_ids, std::ios::binary);
        readXvec<idx_t>(ids_input, origin_ids.data(), batch_size, nbatches);
-       for (size_t i = 0; i < 100; i++){std::cout << origin_ids[i] << " ";}std::cout << std::endl;
+       for (size_t i = 0; i < 20000; i++){std::cout << origin_ids[i] << " ";}std::cout << std::endl;
 
        std::string path_seq_ids = "/home/y/yujianfu/ivf-hnsw/models_VQ/SIFT1M/base_idxs_5000_seq.ivecs";
        std::vector<idx_t> seq_origin_ids(nb);
        std::ifstream seq_ids_input(path_seq_ids, std::ios::binary);
        readXvec<idx_t> (seq_ids_input, seq_origin_ids.data(), batch_size, nbatches);
-       for (size_t i = 0; i < 100; i++){std::cout << seq_origin_ids[i] << " ";}std::cout << std::endl;
+       for (size_t i = 0; i < 20000; i++){std::cout << seq_origin_ids[i] << " ";}std::cout << std::endl;
+
+        //Bslib_Index index = Bslib_Index(dimension, layers, index_type, use_HNSW_VQ, use_norm_quantization);
+        //index.read_index(path_index);
 
 
 
