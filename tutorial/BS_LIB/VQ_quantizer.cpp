2b7288054d4f66fc6e86b68610bd9c03e2228296
Yujian <yujian_fu@outlook.com>
2020-08-02 15:43:58 +0800

    fixed bugs

--- tutorial/BS_LIB/VQ_quantizer.cpp
+++ tutorial/BS_LIB/VQ_quantizer.cpp
@@ -53,14 +53,13 @@ namespace bslib{
         std::cout << "Building group quantizers for vq_quantizer " << std::endl;
         for (size_t i = 0; i < nc_upper; i++){std::cout << train_set[i].size() / 128 << " ";}std::cout << std::endl;
 
-//#pragma omp parallel for
+#pragma omp parallel for
         for (size_t i = 0; i < nc_upper; i++){
             std::vector<float> centroids(dimension * nc_per_group);
             size_t nt_sub = train_set[i].size() / this->dimension;
             std::cout << "Clustering " << nt_sub << " train vectors into " << nc_per_group << " groups " << std::endl;
             faiss::kmeans_clustering(dimension, nt_sub, nc_per_group, train_set[i].data(), centroids.data());
 
-            
             //Adding centroids into quantizers
             if (use_HNSW){
                 hnswlib::HierarchicalNSW * centroid_quantizer = new hnswlib::HierarchicalNSW(dimension, nc_per_group, M, 2 * M, efConstruction);
