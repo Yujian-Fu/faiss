137503646d26dcd1d02e59c2436981893cefb739
Yujian <yujian_fu@outlook.com>
2020-08-03 07:42:56 +0800

    fix bugs

--- tutorial/BS_LIB/parameters/parameters_millions_VQ.h
+++ tutorial/BS_LIB/parameters/parameters_millions_VQ.h
@@ -9,7 +9,7 @@ const size_t layers = 1;
 const size_t VQ_layers = 1;
 const size_t PQ_layers = 0;
 const std::string index_type[layers] = {"VQ"};
-const uint32_t ncentroids[layers] = {5000};
+const uint32_t ncentroids[layers] = {4000};
 
 const bool use_reranking = false;
 const bool use_HNSW_VQ = false;
