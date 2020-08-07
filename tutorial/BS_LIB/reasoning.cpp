133e15545f158e55b9f2a73f03e3de501701dfe7
Yujian <yujian_fu@outlook.com>
2020-08-07 20:30:35 +0800

    add reasoning part

--- tutorial/BS_LIB/reasoning.cpp
+++ tutorial/BS_LIB/reasoning.cpp
@@ -4,10 +4,6 @@
 #include <faiss/IndexFlat.h>
 #include <faiss/Index.h>
 
-typedef faiss::Index::idx_t idx_t;
-using namespace bslib;
-int main(){
-
 //Parameters
     std::string dataset = "SIFT1M";
     std::string model = "VQ";
@@ -36,13 +32,18 @@ int main(){
     std::ifstream base_input(path_base, std::ios::binary);
     std::ifstream gt_input(path_gt, std::ios::binary);
     std::ifstream query_input(path_query, std::ios::binary);
-    std::ofstream record_output(path_record, std::ios::binary);
+    std::ofstream record_output(path_record, std::ios::binary | std::ios::out);
+
+typedef faiss::Index::idx_t idx_t;
+using namespace bslib;
+int main(){
 
     readXvec<float>(train_input, train_set.data(), dimension, train_set_size, false, false);
     readXvec<float>(base_input, base_set.data(), dimension, base_set_size, false, false);
     readXvec<float>(gt_input, gt_set.data(), ngt, base_set_size, false, false);
     readXvec<float> (query_input, query_set.data(), dimension, query_set_size, false, false);
 
+    PrintMessage("Training vectors");
     std::vector<float> centroids(dimension * centroid_num);
     time_recorder trecorder;
     trecorder.reset();
@@ -52,19 +53,34 @@ int main(){
     clus.train (train_set_size, train_set.data(), index);
     
     trecorder.record_time_usage(record_output, "Finish clustering with time usage: ");
+    trecorder.print_time_usage("Finish clustering with time usage: ");
+    trecorder.reset();
     record_output << "Construction parameter: dataset: " << dataset << " train_size: " << train_set_size << " n_centroids: " << centroid_num << " iteration: " << clus.niter << std::endl; 
     std::vector<idx_t> base_assigned_ids(base_set_size); std::vector<float> base_assigned_dis(base_set_size);
     std::vector<idx_t> query_assigned_ids(query_set_size); std::vector<float> query_assigned_dis(query_set_size);
+    std::vector<idx_t> train_assigned_ids(train_set_size); std::vector<float> train_assigned_dis(train_set_size);
+
     index.search(base_set_size, base_set.data(), 1, base_assigned_dis.data(), base_assigned_ids.data());
     index.search(query_set_size, query_set.data(), 1, query_assigned_dis.data(), query_assigned_ids.data());
+    index.search(train_set_size, train_set.data(), 1, train_assigned_dis.data(), train_assigned_ids.data());
+    trecorder.record_time_usage(record_output, "Assigned base vectors and query vectors ");
+    trecorder.print_time_usage("Assigned base vectors and query vectors ");
+
+    std::vector<std::vector<idx_t>> train_assigned_set(centroid_num);
+    for (size_t i = 0; i < train_set_size; i++){train_assigned_set[train_assigned_ids[i]].push_back(i);}
+    float avg_distance = 0;
+    for (size_t i = 0; i < train_set_size; i++){ avg_distance += train_assigned_dis[i];} avg_distance /= train_set_size;
+    record_output << "Avg train distance: " << avg_distance << std::endl;
+    for (size_t i = 0; i < centroid_num; i++){record_output << train_assigned_set[i].size() << " ";} record_output << std::endl;
+
 
     //Quality analysis
     std::vector<std::vector<size_t>> assigned_set(centroid_num);
     for (size_t i = 0; i < base_set_size; i++){assigned_set[base_assigned_ids[i]].push_back(i);}
-    
     const size_t recall_test[3] = {1, 10, 100};
     std::vector<std::vector<size_t>> query_search_result(query_set_size);
 
+    PrintMessage("Analysing clustering quality");
     for (size_t i = 0; i < query_set_size; i++){
         record_output << "Query: " << i << std::endl;
         std::vector<std::vector<size_t>> result_distribution_test(3, std::vector<size_t>(centroid_num, 0));
