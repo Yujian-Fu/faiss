9b392828f5d726b60558f002d4d457ef9543beb0
Yujian <yujian_fu@outlook.com>
2020-08-17 12:12:59 +0800

    add testing

--- tutorial/BS_LIB/module test/PQ_test.cpp
+++ tutorial/BS_LIB/module test/PQ_test.cpp
@@ -40,7 +40,7 @@ int main(){
     int nb = 100000;                       // database size
     int nq = 100;                         // nb of queries
     size_t nlist = 100;
-    size_t M = 8;
+    size_t M = 4;
     size_t nbits = 8;
     size_t nprobe = 100;
     size_t sum_correctness = 0;
