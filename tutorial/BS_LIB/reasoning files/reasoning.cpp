df41a11909747c9975f1344eba4407a00432ec32
Yujian <yujian_fu@outlook.com>
2020-08-24 04:52:56 +0800

    add reasoning

--- tutorial/BS_LIB/reasoning files/reasoning.cpp
+++ tutorial/BS_LIB/reasoning files/reasoning.cpp
@@ -76,12 +76,12 @@ int main(){
     size_t dimension = 256;
     */
 
-    std::string dataset_name = "Random_100";
-    std::string path_list[3] = {"/home/y/yujianfu/ivf-hnsw/data/analysis/Random_10K_100_base.fvecs", "/home/y/yujianfu/ivf-hnsw/data/analysis/Random_10K_100_base.fvecs", "/home/y/yujianfu/ivf-hnsw/data/analysis/Random_10K_100_base.fvecs"};
-    std::string path_query = "/home/y/yujianfu/ivf-hnsw/data/analysis/Random_100_query.fvecs";
+    std::string dataset_name = "Random_400";
+    std::string path_list[3] = {"/home/y/yujianfu/ivf-hnsw/data/analysis/Random_10K_400_base.fvecs", "/home/y/yujianfu/ivf-hnsw/data/analysis/Random_10K_400_base.fvecs", "/home/y/yujianfu/ivf-hnsw/data/analysis/Random_10K_400_base.fvecs"};
+    std::string path_query = "/home/y/yujianfu/ivf-hnsw/data/analysis/Random_400_query.fvecs";
     size_t size_list[3] = {10000, 100000, 1000000};
-    const std::string path_record = "/home/y/yujianfu/ivf-hnsw/data/analysis/Random_100_reasoning.txt";
-    size_t dimension = 100;
+    const std::string path_record = "/home/y/yujianfu/ivf-hnsw/data/analysis/Random_400_reasoning.txt";
+    size_t dimension = 400;
 
    
     const size_t recall_test_size = 3;
