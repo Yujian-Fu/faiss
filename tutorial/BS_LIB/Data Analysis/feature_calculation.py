a85fbe2622afaa59647372247e5b4cc78c00c907
Yujian <yujian_fu@outlook.com>
2020-08-24 03:39:49 +0800

    Add feature calculation

--- tutorial/BS_LIB/Data Analysis/feature_calculation.py
+++ tutorial/BS_LIB/Data Analysis/feature_calculation.py
@@ -9,9 +9,22 @@ from scipy.stats import kurtosis
 from scipy.stats import skew
 
 k_result = 5
-path_list = utils.get_dataset_path_random()
-feature_path = "/home/y/yujianfu/ivf-hnsw/data/analysis/feature_record_random.txt"
+path_list = utils.get_dataset_path_real()
+feature_path = "/home/y/yujianfu/ivf-hnsw/data/analysis/LID_entropy_record_real.txt"
 feature_file = open(feature_path, "w")
+
+
+feature_file.write("Name   LID   Entropy\n")
+for dataset_path in path_list:
+    print("Loading dataset from ", dataset_path)
+    dataset = utils.fvecs_read(dataset_path)
+    print("Computing LID and entropy")
+    entropy = utils.compute_entropy(dataset)
+    LID = utils.compute_LID(dataset)
+    dataset_name = dataset_path.split(".")[0].split("/")[-1]
+    feature_file.write(dataset_name + "   " + str(LID) + "   " + str(entropy) + "\n")
+
+'''
 property_string = "Name   dist_kurtosis   dist_skew   AC   DSC   DC_mean   DC_median   DC_std   TS\n"
 feature_file.write(property_string)
 
@@ -39,9 +52,9 @@ for dataset_path in path_list:
         np.save(dist_path, distance)
         np.save(label_path, labels)
 
-    '''
-    Draw the distance distribution of the NN graph
-    '''
+    
+    #Draw the distance distribution of the NN graph
+    
     print("Computing the skewness and the kurtosis")
     x = []
     for i in range(dataset.shape[0]):
@@ -60,9 +73,9 @@ for dataset_path in path_list:
     fig_path = dataset_path.split(".")[0] + "_distance.png"
     plt.savefig(fig_path)
 
-    '''
-    Compute the LID and entropy of the dataset
-    '''
+    
+    #Compute the LID and entropy of the dataset
+    
     #print("Computing the LID and entropy")
     #LID = utils.compute_LID(dataset)
     #entropy = utils.compute_entropy(dataset)
@@ -77,19 +90,19 @@ for dataset_path in path_list:
             DG.add_weighted_edges_from([(i, labels[i][j + 1], distance[i][j + 1])])
 
 
-    '''
-    G = nx.Graph()
-    G.add_nodes_from(range(dataset.shape[0]))
+    
+    #G = nx.Graph()
+    #G.add_nodes_from(range(dataset.shape[0]))
 
-    for i in range(dataset.shape[0]):
-        for j in range(k_result):
-            G.add_edges_from([(i, labels[j + 1])])
-    '''
+    #for i in range(dataset.shape[0]):
+    #    for j in range(k_result):
+    #        G.add_edges_from([(i, labels[j + 1])])
+    
 
-    '''
-    Properties from networkX
-    Re-check the requirement of those features 
-    '''
+    
+    #Properties from networkX
+    #Re-check the requirement of those features 
+    
 
     print("Computing the graph features")
     AC = nx.average_clustering(DG)
@@ -127,7 +140,7 @@ for dataset_path in path_list:
     result_string = dataset_name +"   "+str(dist_kurtosis)+"   "+str(dist_skew)+"   "+str(AC)+"   "+str(DSC)+"   "+str(DC_mean)+"   "+str(DC_median)+"   "+str(DC_std)+"   "+str(TS)+"\n"
     print(result_string)
     feature_file.write(result_string)
-
+'''
 
 
 
