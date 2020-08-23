25dfd9d2d29485ba209627ad85253862d8da559f
Yujian <yujian_fu@outlook.com>
2020-08-23 22:25:18 +0800

    Add data generation

--- tutorial/BS_LIB/Data Analysis/data_preparation.py
+++ tutorial/BS_LIB/Data Analysis/data_preparation.py
@@ -6,7 +6,7 @@
 import numpy as np 
 import random 
 import utils 
-
+'''
 real_dataset_list = ["SIFT", "GIST", "DEEP"]
 size_list = ["10K", "100K"]
 
@@ -15,7 +15,7 @@ dataset_path = "/home/y/yujianfu/ivf-hnsw/data/"
 for dataset in real_dataset_list:
     for size in size_list:
         dataset_file = dataset_path + dataset + "1M/" + dataset + "1M_base.fvecs" 
-        sample_dataset_file = dataset_path + "analysis/" + dataset + "_" + size + ".fvecs"
+        sample_dataset_file = dataset_path + "analysis/" + dataset + size +"_base" + ".fvecs"
         print("read dataset from ", dataset_file)
         real_dataset = utils.fvecs_read(dataset_file)
         sample_size = int(size.split("K")[0])
@@ -24,7 +24,7 @@ for dataset in real_dataset_list:
         index = random.sample(range(real_dataset.shape[0]), sample_size * 1000)
         sample_dataset = real_dataset[index, :]
         utils.fvecs_write(sample_dataset_file, sample_dataset)
-
+'''
 
 size_list = ["10K", "100K", "1000K", "10000K"]
 
@@ -33,11 +33,12 @@ mu = 10
 sigma = 0.1
 for size in size_list:
     for dimension in range(100, 1000, 100):
-        sample_size = float(size.split("K")[0])
-        sample_dataset_file = dataset_path + "analysis/" + dataset + "_" + sample_size + "_" + dimension + ".fvecs"
+        sample_size = float(size.split("K")[0]) 
+        sample_dataset_file = dataset_path + "analysis/" + dataset + "_" + str(sample_size) + "K_" + dimension + ".fvecs"
+        print("Generating dataset to ", sample_dataset_file)
         dataset_file = dataset_path + dataset + size + dimension
-        sample_dataset = np.zeros((sample_size, dimension))
-        for i in range(sample_size):
+        sample_dataset = np.zeros((sample_size * 1000, dimension))
+        for i in range(sample_size * 1000):
             sample_dataset[i, :] = np.random.normal(mu, sigma, dimension)
         utils.fvecs_write(sample_dataset_file, sample_dataset)
 
@@ -45,11 +46,12 @@ for size in size_list:
 dataset = "Random"
 for size in size_list:
     for dimension in range(100, 1000, 100):
-        sample_size = float(size.split("K")[0])
-        sample_dataset_file = dataset_path + "analysis/" + dataset + "_" + sample_size + "_" + dimension + ".fvecs"
+        sample_size = float(size.split("K")[0]) * 1000
+        sample_dataset_file = dataset_path + "analysis/" + dataset + "_" + str(sample_size) + "K_" + dimension + ".fvecs"
+        print("Generating dataset to ", sample_dataset_file)
         dataset_file = dataset_path + dataset + size + dimension
-        sample_dataset = np.zeros((sample_size, dimension))
-        for i in range(sample_size):
+        sample_dataset = np.zeros((sample_size* 1000, dimension))
+        for i in range(sample_size* 1000):
             sample_dataset[i, :] = np.random.randint(0, 100, (sample_size, dimension))
         utils.fvecs_write(sample_dataset_file, sample_dataset)
 
