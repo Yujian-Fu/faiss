def02d4b83e907a69fd03e5ff08233ed868691e2
Yujian <yujian_fu@outlook.com>
2020-08-24 03:10:12 +0800

    Add feature calculation

--- tutorial/BS_LIB/Data Analysis/feature_calculation.py
+++ tutorial/BS_LIB/Data Analysis/feature_calculation.py
@@ -12,7 +12,7 @@ k_result = 5
 path_list = utils.get_dataset_path_real()
 feature_path = "/home/y/yujianfu/ivf-hnsw/data/analysis/feature_record_real.txt"
 feature_file = open(feature_path, "w")
-property_string = "LID   entropy   dist_kurtosis   dist_skew   AC   DSC   DC_mean   DC_median   DC_std   TS\n"
+property_string = "Name   LID   entropy   dist_kurtosis   dist_skew   AC   DSC   DC_mean   DC_median   DC_std   TS\n"
 feature_file.write(property_string)
 
 for dataset_path in path_list:
@@ -124,7 +124,7 @@ for dataset_path in path_list:
     #Not implemented for directed type
     #OMEGA = nx.omega(DG)
 
-    result_string = str(LID)+"   "+str(entropy)+"   "+str(dist_kurtosis)+"   "+str(dist_skew)+"   "+str(AC)+"   "+str(DSC)+"   "+str(DC_mean)+"   "+str(DC_median)+"   "+str(DC_std)+"   "+str(TS)+"\n"
+    result_string = dataset_name +"   " str(LID)+"   "+str(entropy)+"   "+str(dist_kurtosis)+"   "+str(dist_skew)+"   "+str(AC)+"   "+str(DSC)+"   "+str(DC_mean)+"   "+str(DC_median)+"   "+str(DC_std)+"   "+str(TS)+"\n"
     feature_file.write(result_string)
 
 
