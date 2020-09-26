1b3b81b678e131f4d30a8e5dc37f8eb96af5bd85
Yujian <yujian_fu@outlook.com>
2020-09-26 16:57:07 +0800

    test billion scale dataset

--- tutorial/BS_LIB/parameters/parameters_PQ.h
+++ tutorial/BS_LIB/parameters/parameters_PQ.h
@@ -15,7 +15,7 @@ const bool use_reranking = false;
 const bool use_HNSW_VQ = false;
 const bool use_norm_quantization = false;
 const bool use_dynamic_reranking = false;
-const bool use_OPQ = true;
+const bool use_OPQ = false;
 const bool use_parallel_indexing = false;
 
 //For train PQ
