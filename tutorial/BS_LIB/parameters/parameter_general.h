c313f5dec8d9602f508d717b89b22cdc11669f5a
Yujian <yujian_fu@outlook.com>
2020-10-02 02:18:14 +0800

    test parameter prediction

--- tutorial/BS_LIB/parameters/parameter_general.h
+++ tutorial/BS_LIB/parameters/parameter_general.h
@@ -2,7 +2,7 @@
 
 const std::string base_path = "/home/y/yujianfu/ivf-hnsw/";
 //const std::string base_path = "/home/yujian/Desktop/extra/Similarity Search/similarity_search_datasets/";
-const size_t M_PQ = 16;
+const size_t M_PQ = 32;
 const std::string dataset = "DEEP1M";
 const size_t dimension = 256;
 
