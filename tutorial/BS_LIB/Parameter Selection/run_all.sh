4ac679181f1996539221fd60a3aee32b952ff93c
Yujian <yujian_fu@outlook.com>
2020-10-02 04:08:01 +0800

    test parameter prediction

--- tutorial/BS_LIB/Parameter Selection/run_all.sh
+++ tutorial/BS_LIB/Parameter Selection/run_all.sh
@@ -2,7 +2,7 @@
 #---------------
 #Run all other files
 #---------------
-echo 'Running all scripts for DEEP1M dataset with 32 bytes'
+echo 'Running all scripts for SIFT1M dataset with 8 bytes'
 
 echo 'Running Inverted Index'
 source inverted_index.sh
