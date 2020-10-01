ba6872ae87ba897ef28becb349b141cf69e0fc38
Yujian <yujian_fu@outlook.com>
2020-10-02 04:51:30 +0800

    billion scale index

--- tutorial/BS_LIB/bslib.cpp
+++ tutorial/BS_LIB/bslib.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 #include "bslib_index.h"
-#include "./parameters/parameters_million/parameters_millions_VQ_LQ.h"
+#include "./parameters/parameters_billion/parameters_PQ.h"
 
 using namespace bslib;
 
