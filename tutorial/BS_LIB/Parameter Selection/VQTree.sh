87f226f896e76f6cdaacf97a423f61557cc89957
Yujian <yujian_fu@outlook.com>
2020-10-03 18:08:05 +0800

    test SIFT10K dataset

--- tutorial/BS_LIB/Parameter Selection/VQTree.sh
+++ tutorial/BS_LIB/Parameter Selection/VQTree.sh
@@ -1,9 +1,9 @@
 #！/bin/bash
 #This is for running inverted index from 200 to 5000
 count=0
-for i in {50..200..20}
+for i in {10..50..5}
 do
-    for j in {50..200..20}
+    for j in {10..50..5}
     do
         ./VQTree $i $j $count
         let count=$count+1
