ef744933fd7dffabe85ff150c37ceacb2f739532
Yujian <yujian_fu@outlook.com>
2020-10-03 17:57:56 +0800

    test DEEP1M dataset

--- tutorial/BS_LIB/Parameter Selection/ICI.sh
+++ tutorial/BS_LIB/Parameter Selection/ICI.sh
@@ -1,9 +1,10 @@
 #！/bin/bash
 #This is for running inverted index from 200 to 5000
 count=0
-for i in {4..128..4}
+#for i in {4..128..4}
+for i in {4..32..2}
 do
-    for j in {4..8..1}
+    for j in {3..6..1}
     do
         ./ICI $i $j $count
         let count=$count+1
