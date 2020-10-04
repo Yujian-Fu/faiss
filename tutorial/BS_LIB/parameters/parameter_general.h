a4e1e3a17e2fc98a8b8ddc5f88eca7db56439f1a
Yujian <yujian_fu@outlook.com>
2020-10-04 20:08:13 +0800

    Test GIST1M

--- tutorial/BS_LIB/parameters/parameter_general.h
+++ tutorial/BS_LIB/parameters/parameter_general.h
@@ -1,10 +1,10 @@
 #include <string>
 
-//const std::string base_path = "/home/y/yujianfu/ivf-hnsw/";
-const std::string base_path = "/home/yujian/Desktop/extra/Similarity Search/similarity_search_datasets/";
+const std::string base_path = "/home/y/yujianfu/ivf-hnsw/";
+//const std::string base_path = "/home/yujian/Desktop/extra/Similarity Search/similarity_search_datasets/";
 
-const std::string dataset = "SIFT1M";
-const size_t dimension = 128;
+const std::string dataset = "GIST1M";
+const size_t dimension = 960;
 
 //For train PQ
 const size_t M_PQ = 16;
