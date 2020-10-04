795a8093fbbef0575e10da3ee01e947c91bb02c2
Yujian <yujian_fu@outlook.com>
2020-10-05 04:58:34 +0800

    Test GIST1M

--- tutorial/BS_LIB/Parameter Selection/GIST1M/VQTree.sh
+++ tutorial/BS_LIB/Parameter Selection/GIST1M/VQTree.sh
@@ -2,8 +2,8 @@
 #This is for running inverted index from 200 to 5000
 count=0
 
-#for i in {10..50..5}
-for i in {10..30..5}
+for i in {10..50..5}
+#for i in {10..30..5}
 do
     #for j in {10..50..5}
     for j in {10..30..5}
