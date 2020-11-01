828e7164d626373c6ca4687d4cd44eb3d0cba37a
Yujian <yujian_fu@outlook.com>
2020-11-01 22:28:46 +0800

    build SIFT datasets

--- tutorial/BS_LIB/Data Analysis/data_preparation_SIFT.py
+++ tutorial/BS_LIB/Data Analysis/data_preparation_SIFT.py
@@ -20,36 +20,42 @@ def create_dir_not_exist(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+print("read dataset from ", billion_dataset_base)
+real_base_dataset = utils.bvecs_read(billion_dataset_base)
+
 for size in size_list:
     folder_path = dataset_path + "SIFT" + size
     create_dir_not_exist(dataset_path + "SIFT" + size)
     
-    print("read dataset from ", billion_dataset_base)
-    real_base_dataset = utils.bvecs_read(billion_dataset_base)
+
     if "K" in size:
         sample_size = int(size.split("K")[0]) * 1000
     if "M" in size:
         sample_size = int(size.split("M")[0]) * 1000000
     print("The file size is: ", sample_size)
     
+    '''
     index = random.sample(range(real_base_dataset.shape[0]), sample_size)
     base_dataset = real_base_dataset[index, :]
-    base_dataset_file = folder_path + "/SIFT" + size +"_base" + ".fvecs"
     utils.fvecs_write(base_dataset_file, base_dataset)
     print("Write base file")
+    '''
+
+    base_dataset_file = folder_path + "/SIFT" + size +"_base" + ".fvecs"
+    base_dataset = utils.fvecs_read(base_dataset_file)
 
     real_learn_dataset = utils.bvecs_read(billion_dataset_learn)
     index = random.sample(range(real_learn_dataset.shape[0]), int(sample_size / 10))
     learn_dataset = real_learn_dataset[index, :]
     learn_dataset_file = folder_path + "/SIFT" + size +"_learn" + ".fvecs"
-    utils.fvecs_write(learn_dataset, learn_dataset_file)
+    utils.fvecs_write(learn_dataset_file, learn_dataset)
     print("Write learn file")
 
     real_query_dataset = utils.bvecs_read(billion_dataset_query)
     index = random.sample(range(real_query_dataset.shape[0]), nq)
     query_dataset = real_query_dataset[index, :]
     query_dataset_file = folder_path + "/SIFT" + size + "_query" + ".fvecs"
-    utils.fvecs_write(query_dataset, query_dataset_file)
+    utils.fvecs_write(query_dataset_file, query_dataset)
     print("Write query file")
 
     assert(base_dataset.shape[1] == dimension)
@@ -57,7 +63,7 @@ for size in size_list:
     index.add(base_dataset)
     D, I = index.search(query_dataset, ngt)
     groundtruth_file = folder_path + "/SIFT" + size + "_groundtruth" + ".fvecs"
-    utils.fvecs_write(I, groundtruth_file)
+    utils.fvecs_write(groundtruth_file, I)
     print("Write GT file")
 
 
