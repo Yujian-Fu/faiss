d8983bff414908fa54e7da8b8578b6b39cd4d52d
Yujian <yujian_fu@outlook.com>
2020-11-29 09:55:53 +0800

    test

--- tutorial/BS_LIB/Multi-Train/parameter_multi.h
+++ tutorial/BS_LIB/Multi-Train/parameter_multi.h
@@ -23,8 +23,8 @@ const size_t N_random = 999;
 const bool use_kmeansplusplus = false;
 
 const size_t index_iter = 1;
-const size_t PQ_iter = 1;
-const size_t total_iter = 50;
+const size_t PQ_iter = 3;
+const size_t total_iter = 25;
 std::vector<size_t> recall_k_list = {1, 10, 100};
 
 
