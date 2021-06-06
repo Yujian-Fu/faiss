3b69ce826cc8ceb10c8ddc8782552b11b0e117c6
Yujian <yujian_fu@outlook.com>
2021-06-06 17:24:08 +0800

    test billion scale

--- tutorial/BS_LIB/parameters/parameters_billion/parameters_VQ_VQ.h
+++ tutorial/BS_LIB/parameters/parameters_billion/parameters_VQ_VQ.h
@@ -17,10 +17,10 @@ const size_t efSearch[VQ_layers] = {};
 
 const size_t M_PQ_layer[PQ_layers] = {};
 const size_t nbits_PQ_layer[PQ_layers] = {};
-const size_t num_train[layers] = {100000, 10000000};
+const size_t num_train[layers] = {100000, 1000000};
 
 //For searching
-const size_t keep_space[layers * num_search_paras] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+const size_t keep_space[layers * num_search_paras] = {10, 10, 20, 10, 10, 20, 20, 30, 30, 20, 40, 10, 50, 10, 50, 20, 60, 10, 60, 20};
 
 std::string conf_combination(){
     std::string result = "";
