7d00d712527af79d1e7e5eac0748505c5d21e7bf
Yujian <yujian_fu@outlook.com>
2021-07-05 00:09:24 +0800

    add feature

--- tutorial/BS_LIB/HNSWlib/hnswalg.cpp
+++ tutorial/BS_LIB/HNSWlib/hnswalg.cpp
@@ -54,7 +54,7 @@ namespace hnswlib {
     data_level0_memory_ = (char *) malloc(maxelements_ * size_data_per_element);
     std::cout << (data_level0_memory_ ? " Memory allocated " : " Memory not allocated ") << std::endl;
 
-    std::cout << "HNSW Size Mb: " << (maxelements_ * size_data_per_element) / (1000 * 1000) << std::endl;
+    std::cout << "HNSW Size Kb: " << (maxelements_ * size_data_per_element) / (1000) << std::endl;
 
     visitedlistpool = new VisitedListPool(1, maxelements_);
 
