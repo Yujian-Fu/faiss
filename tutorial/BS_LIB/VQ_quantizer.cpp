6f5d793ba0b646d4ba36d7c5c963155b5b29873e
Yujian <yujian_fu@outlook.com>
2021-07-05 00:14:36 +0800

    add feature

--- tutorial/BS_LIB/VQ_quantizer.cpp
+++ tutorial/BS_LIB/VQ_quantizer.cpp
@@ -86,8 +86,8 @@ namespace bslib{
             if (use_HNSW){
                 hnswlib::HierarchicalNSW * centroid_quantizer = new hnswlib::HierarchicalNSW(dimension, exact_nc_in_group, M, 2 * M, efConstruction);
                 for (size_t j = 0; j < exact_nc_in_group; j++){
-                    centroid_quantizer->addPoint(centroids.data() + j * dimension);
                     std::cout << j << " ";
+                    centroid_quantizer->addPoint(centroids.data() + j * dimension);
                 }
                 this->HNSW_quantizers[i] = centroid_quantizer;
             }
