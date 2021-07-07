1fafc11bfc18fe7db22df05d5b3684a50334e064
Yujian <yujian_fu@outlook.com>
2021-07-07 20:03:50 +0800

    add feature

--- tutorial/BS_LIB/bslib_index.cpp
+++ tutorial/BS_LIB/bslib_index.cpp
@@ -1948,6 +1948,7 @@ namespace bslib{
                                 uint32_t size = *ll_cur;
                                 group_HNSW_output.write((char *) &size, sizeof(uint32_t));
                                 hnswlib::idx_t *data = (hnswlib::idx_t *)(ll_cur + 1);
+                                std::cout << temp << std::endl;
                                 group_HNSW_output.write((char *) data, sizeof(hnswlib::idx_t) * size);
                             }
                             delete [] group_HNSW;
