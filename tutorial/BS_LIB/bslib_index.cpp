2e5f4cf8f5d37705762fd1535471a975d24d97cc
Yujian <yujian_fu@outlook.com>
2021-07-09 02:08:02 +0800

    add feature

--- tutorial/BS_LIB/bslib_index.cpp
+++ tutorial/BS_LIB/bslib_index.cpp
@@ -1467,7 +1467,7 @@ namespace bslib{
                 if (LQ_type != 2){
                     for (size_t group_id = 0; group_id < nc_upper; group_id++){
                         quantizer_input.read((char *) alphas.data(), max_nc_per_group * sizeof(float));
-                        for (size_t inner_group_id = 0; inner_group_id < nc_upper; inner_group_id++){
+                        for (size_t inner_group_id = 0; inner_group_id < max_nc_per_group; inner_group_id++){
                             lq_quantizer.alphas[group_id][inner_group_id] = alphas[inner_group_id];
                         }
                     }
