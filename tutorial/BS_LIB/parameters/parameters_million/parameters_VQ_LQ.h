324cc6cc4304d5370077ff67eccd0ab2ba6413fa
Yujian <yujian_fu@outlook.com>
2021-07-10 23:37:45 +0800

    add feature

--- tutorial/BS_LIB/parameters/parameters_million/parameters_VQ_LQ.h
+++ tutorial/BS_LIB/parameters/parameters_million/parameters_VQ_LQ.h
@@ -4,18 +4,18 @@
 //Exp parameters
 //For index initialization
 
-const size_t VQ_layers = 3;
+const size_t VQ_layers = 2;
 const size_t PQ_layers = 0;
-const size_t LQ_layers = 0;
+const size_t LQ_layers = 1;
 const size_t layers = VQ_layers + PQ_layers + LQ_layers;
-const size_t LQ_type[LQ_layers] = {};
+const size_t LQ_type[LQ_layers] = {0};
 
-const std::string index_type[layers] = {"VQ", "VQ", "VQ"};
-const uint32_t ncentroids[layers-PQ_layers] = {20, 20, 20};
+const std::string index_type[layers] = {"VQ", "VQ", "LQ"};
+const uint32_t ncentroids[layers-PQ_layers] = {40, 40, 20};
 
 
 //For building index
-bool use_HNSW_VQ[VQ_layers] = {false};
+bool use_HNSW_VQ[VQ_layers] = {false, false};
 const size_t M_HNSW[VQ_layers] = {4};
 //Set efConstruction and efSearch as the same
 const size_t efConstruction [VQ_layers] = {10};
