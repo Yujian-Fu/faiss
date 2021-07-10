22a5b80d84447aa754877971e43fbb091490e37e
Yujian <yujian_fu@outlook.com>
2021-07-10 17:00:18 +0800

    add feature

--- tutorial/BS_LIB/parameters/parameters_million/parameters_VQ_LQ.h
+++ tutorial/BS_LIB/parameters/parameters_million/parameters_VQ_LQ.h
@@ -15,13 +15,17 @@ const uint32_t ncentroids[layers-PQ_layers] = {1000, 64};
 
 
 //For building index
-const size_t M_HNSW[VQ_layers] = {};
-const size_t efConstruction [VQ_layers] = {};
-const size_t efSearch[VQ_layers] = {};
+bool use_HNSW_VQ[VQ_layers] = {false};
+const size_t M_HNSW[VQ_layers] = {4};
+//Set efConstruction and efSearch as the same
+const size_t efConstruction [VQ_layers] = {10};
+
+const size_t M_HNSW_all[VQ_layers] = {};
+const size_t efConstruction_all [VQ_layers] = {};
 
 const size_t M_PQ_layer[PQ_layers] = {};
 const size_t nbits_PQ_layer[PQ_layers] = {};
-const size_t num_train[layers] = {100000, 10000};
+const size_t num_train[layers] = {10000, 100000};
 
 //For searching
 const size_t keep_space[layers * num_search_paras] = {1000, 64};
