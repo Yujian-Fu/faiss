3cdf663aac16583eed2481093eacabc88c544e36
Yujian <yujian_fu@outlook.com>
2021-07-10 23:42:38 +0800

    add feature

--- tutorial/BS_LIB/parameters/parameters_million/parameters_VQ_LQ.h
+++ tutorial/BS_LIB/parameters/parameters_million/parameters_VQ_LQ.h
@@ -4,18 +4,18 @@
 //Exp parameters
 //For index initialization
 
-const size_t VQ_layers = 2;
-const size_t PQ_layers = 0;
-const size_t LQ_layers = 1;
+const size_t VQ_layers = 1;
+const size_t PQ_layers = 1;
+const size_t LQ_layers = 0;
 const size_t layers = VQ_layers + PQ_layers + LQ_layers;
-const size_t LQ_type[LQ_layers] = {0};
+const size_t LQ_type[LQ_layers] = {};
 
-const std::string index_type[layers] = {"VQ", "VQ", "LQ"};
-const uint32_t ncentroids[layers-PQ_layers] = {40, 40, 20};
+const std::string index_type[layers] = {"VQ", "PQ"};
+const uint32_t ncentroids[layers-PQ_layers] = {40};
 
 
 //For building index
-bool use_HNSW_VQ[VQ_layers] = {false, false};
+bool use_HNSW_VQ[VQ_layers] = {false};
 const size_t M_HNSW[VQ_layers] = {4};
 //Set efConstruction and efSearch as the same
 const size_t efConstruction [VQ_layers] = {10};
@@ -23,10 +23,10 @@ const size_t efConstruction [VQ_layers] = {10};
 const size_t M_HNSW_all[VQ_layers] = {};
 const size_t efConstruction_all [VQ_layers] = {};
 
-const size_t M_PQ_layer[PQ_layers] = {};
-const size_t nbits_PQ_layer[PQ_layers] = {};
-const size_t num_train[layers] = {100000, 100000, 100000};
+const size_t M_PQ_layer[PQ_layers] = {2};
+const size_t nbits_PQ_layer[PQ_layers] = {8};
+const size_t num_train[layers] = {100000, 100000};
 
 //For searching
-const size_t keep_space[layers * num_search_paras] = {20, 20, 20};
+const size_t keep_space[layers * num_search_paras] = {20, 1000};
 
