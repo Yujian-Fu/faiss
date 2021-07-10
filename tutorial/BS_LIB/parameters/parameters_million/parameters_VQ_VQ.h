f84585ad73c1fffa0eeae73d1a953c8b5346cab0
Yujian <yujian_fu@outlook.com>
2021-07-10 17:29:41 +0800

    add feature

--- tutorial/BS_LIB/parameters/parameters_million/parameters_VQ_VQ.h
+++ tutorial/BS_LIB/parameters/parameters_million/parameters_VQ_VQ.h
@@ -11,18 +11,21 @@ const size_t layers = VQ_layers + PQ_layers + LQ_layers;
 const size_t LQ_type[LQ_layers] = {};
 
 const std::string index_type[layers] = {"VQ", "VQ"};
-const uint32_t ncentroids[layers] = {200, 10};
+const uint32_t ncentroids[layers] = {1000, 64};
 
 
 //For building index
-const size_t M_HNSW[VQ_layers] = {};
-const size_t efConstruction [VQ_layers] = {};
-const size_t efSearch[VQ_layers] = {};
+bool use_HNSW_VQ[VQ_layers] = {false, false};
+const size_t M_HNSW[VQ_layers] = {4};
+//Set efConstruction and efSearch as the same
+const size_t efConstruction [VQ_layers] = {10};
+
+const size_t M_HNSW_all[VQ_layers] = {};
+const size_t efConstruction_all [VQ_layers] = {};
 
 const size_t M_PQ_layer[PQ_layers] = {};
 const size_t nbits_PQ_layer[PQ_layers] = {};
-const size_t num_train[layers] = {100000, 100000};
+const size_t num_train[layers] = {10000, 100000};
 
 //For searching
-const size_t keep_space[layers * num_search_paras] = {10, 2, 10, 4, 10, 6, 20, 2, 20, 4, 30, 6, 40, 2, 40, 4, 40, 6, 
-    50, 2};
+const size_t keep_space[layers * num_search_paras] = {1000, 64};
