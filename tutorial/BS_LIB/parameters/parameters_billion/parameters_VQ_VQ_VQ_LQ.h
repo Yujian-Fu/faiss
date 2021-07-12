7a21c5cd6b4f90b2db3c403d7b4c7b557329a5ad
Yujian <yujian_fu@outlook.com>
2021-07-13 05:00:55 +0800

    final result

--- tutorial/BS_LIB/parameters/parameters_billion/parameters_VQ_VQ_VQ_LQ.h
+++ tutorial/BS_LIB/parameters/parameters_billion/parameters_VQ_VQ_VQ_LQ.h
@@ -4,24 +4,26 @@
 //Exp parameters
 //For index initialization
 
-const size_t VQ_layers = 1;
-const size_t PQ_layers = 1;
-const size_t LQ_layers = 0;
+const size_t VQ_layers = 3;
+const size_t PQ_layers = 0;
+const size_t LQ_layers = 1;
 const size_t layers = VQ_layers + PQ_layers + LQ_layers;
-const size_t LQ_type[LQ_layers] = {};
+const size_t LQ_type[LQ_layers] = {0};
 
-const std::string index_type[layers] = {"VQ", "PQ"};
-const uint32_t ncentroids[layers-PQ_layers] = {1000};
-
+const std::string index_type[layers] = {"VQ", "VQ", "VQ", "LQ"};
+const uint32_t ncentroids[layers-PQ_layers] = {100, 100, 1000, 50};
 
 //For building index
+bool use_HNSW_VQ[VQ_layers] = {false, false, false};
 const size_t M_HNSW[VQ_layers] = {};
 const size_t efConstruction [VQ_layers] = {};
-const size_t efSearch[VQ_layers] = {};
 
-const size_t M_PQ_layer[PQ_layers] = {2};
-const size_t nbits_PQ_layer[PQ_layers] = {10};
-const size_t num_train[layers] = {500000, 100000000};
+const size_t M_HNSW_all[VQ_layers] = {};
+const size_t efConstruction_all [VQ_layers] = {};
+
+const size_t M_PQ_layer[PQ_layers] = {};
+const size_t nbits_PQ_layer[PQ_layers] = {};
+const size_t num_train[layers] = {100000000, 100000000, 100000000, 100000000};
 
 //For searching
-const size_t keep_space[layers * num_search_paras] = {50, 10, 100, 10, 150, 20, 200, 30, 250, 20, 300, 10, 350, 10, 400, 20, 450, 10, 500, 20};
+const size_t keep_space[layers * num_search_paras] = {20, 20, 100, 20};
