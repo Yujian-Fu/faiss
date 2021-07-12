cc593f50e1703174b23543901bc2d0af5aebf826
Yujian <yujian_fu@outlook.com>
2021-07-13 04:51:07 +0800

    final test

--- tutorial/BS_LIB/parameters/parameters_billion/parameters_VQ_PQ.h
+++ tutorial/BS_LIB/parameters/parameters_billion/parameters_VQ_PQ.h
@@ -15,13 +15,17 @@ const uint32_t ncentroids[layers-PQ_layers] = {1000};
 
 
 //For building index
-const size_t M_HNSW[VQ_layers] = {};
-const size_t efConstruction [VQ_layers] = {};
-const size_t efSearch[VQ_layers] = {};
+bool use_HNSW_VQ[VQ_layers] = {false};
+const size_t M_HNSW[VQ_layers] = {4};
+//Set efConstruction and efSearch as the same
+const size_t efConstruction [VQ_layers] = {10};
+
+const size_t M_HNSW_all[VQ_layers] = {};
+const size_t efConstruction_all [VQ_layers] = {};
 
 const size_t M_PQ_layer[PQ_layers] = {2};
-const size_t nbits_PQ_layer[PQ_layers] = {10};
-const size_t num_train[layers] = {500000, 100000000};
+const size_t nbits_PQ_layer[PQ_layers] = {11};
+const size_t num_train[layers] = {10000000, 100000000};
 
 //For searching
-const size_t keep_space[layers * num_search_paras] = {50, 10, 100, 10, 150, 20, 200, 30, 250, 20, 300, 10, 350, 10, 400, 20, 450, 10, 500, 20};
+const size_t keep_space[layers * num_search_paras] = {100, 200};
