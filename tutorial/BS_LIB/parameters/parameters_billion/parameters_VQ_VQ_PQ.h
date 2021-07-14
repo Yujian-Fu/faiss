cae50af47e9378fadc8a8530b4f29a9acb1a5a0c
Yujian <yujian_fu@outlook.com>
2021-07-14 09:31:52 +0800

    final test

--- tutorial/BS_LIB/parameters/parameters_billion/parameters_VQ_VQ_PQ.h
+++ tutorial/BS_LIB/parameters/parameters_billion/parameters_VQ_VQ_PQ.h
@@ -14,13 +14,16 @@ const uint32_t ncentroids[layers-PQ_layers] = {100, 100};
 
 
 //For building index
+bool use_HNSW_VQ[VQ_layers] = {false, false};
 const size_t M_HNSW[VQ_layers] = {};
 const size_t efConstruction [VQ_layers] = {};
-const size_t efSearch[VQ_layers] = {};
+
+const size_t M_HNSW_all[VQ_layers] = {};
+const size_t efConstruction_all [VQ_layers] = {};
 
 const size_t M_PQ_layer[PQ_layers] = {2};
-const size_t nbits_PQ_layer[PQ_layers] = {6};
-const size_t num_train[layers] = {10000, 100000, 50000000};
+const size_t nbits_PQ_layer[PQ_layers] = {8};
+const size_t num_train[layers] = {300000, 100000000, 100000000};
 
 //For searching
 const size_t keep_space[layers * num_search_paras] = {10, 10, 20, 10, 10, 20, 20, 30, 30, 20, 40, 10, 50, 10, 50, 20, 60, 10, 60, 20};
